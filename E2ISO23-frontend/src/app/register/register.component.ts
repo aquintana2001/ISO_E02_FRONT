85c15b87b8cea4cbaf8ee412907e5d9932f5e841
GuillermoSantosMolero <115094288+GuillermoSantosMolero@users.noreply.github.com>
2023-10-21 13:40:25 +0200

    0.1.1
    Se ha añadido el componente register y el documento de sonar-project.properties

--- E2ISO23-frontend/src/app/register/register.component.ts
+++ E2ISO23-frontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    'username': new FormControl('', Validators.required),
+    'password': new FormControl('', Validators.required)
+ });
 
+ onSubmit() {
+    // Add your login logic here
+    console.log('Login Form Submitted', this.registerForm.value);
+ }
 }
