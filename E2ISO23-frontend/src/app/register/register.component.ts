73bcfb36ea7a986177b9ccc05e197d4945290c5a
aquintana2001 <Alejandro.Quintana@alu.uclm.es>
2023-10-23 10:22:07 +0200

    Lógica registro
    Realizada la clase .ts del registro y conectado con el back en el user.service.ts

--- E2ISO23-frontend/src/app/register/register.component.ts
+++ E2ISO23-frontend/src/app/register/register.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from '../user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent{
+  nombre?: String
+  apellidos?: String
+  email?: String
+  password1?: String
+  password2?: String
+  ciudad?: String
+  carnet?: boolean
+  telefono?: String
+  dni?: String
 
+  constructor(private userService : AccountService) { 
+  }
+
+  ngOnInit(): void {
+  }
+
+  register(){
+    let info = {
+      nombre : this.nombre,
+      apellidos:this.apellidos,
+      email:this.email,
+      password1:this.password1,
+      password2:this.password2,
+      ciudad:this.ciudad,
+      carnet:this.carnet,
+      telefono:this.telefono,
+      dni:this.dni
+    }
+
+    this.userService.register(info).subscribe(
+      respuesta =>{
+      alert(respuesta)
+      console.log(respuesta)
+    },error => {
+      alert(error.error.message)
+    })
+  }
 }
