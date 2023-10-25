938822b49aa58bd5ed3033e53eb7def6ac1cb1c5
Noel Fernández de Sevilla Sánchez <Noel.Fernandez@alu.uclm.es>
2023-10-25 13:32:16 +0200

    Añadidas carpeta y rutas de Consultar Usuarios

---
+++ E2ISO23-frontend/src/app/consultar-usuarios/consultar-usuarios.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+ selector: 'app-consultar-usuarios',
+ templateUrl: './consultar-usuarios.component.html',
+ styleUrls: ['./consultar-usuarios.component.css']
+})
+export class ConsultarUsuariosComponent {
+ consultarUsuariosForm = new FormGroup({
+   
+ });
+
+
+}
