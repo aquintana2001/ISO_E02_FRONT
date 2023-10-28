70d3440b30c0241df22264f95e5384d0ccc527f4
Noel Fernández de Sevilla Sánchez <Noel.Fernandez@alu.uclm.es>
2023-10-28 20:08:51 +0200

    Implementación Modificar usuarios y eliminar parcialmente

--- E2ISO23-frontend/src/app/user.service.ts
+++ E2ISO23-frontend/src/app/user.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ConsultarUsuariosComponent } from './consultar-usuarios/consultar-usuarios.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private baseURLAdmin = "http://localhost:8080/admin/cliente";
+  private baseURLAdminActualizarCliente = "http://localhost:8080/admin/actualizarCliente";
+  private baseURLAdminEliminarCliente = "http://localhost:8080/admin/eliminarCliente";
   private baseURLAdminVehiculos = "http://localhost:8080/admin/vehiculo";
 
-
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   getClientes(): Observable<any[]> {
     return this.httpClient.get<any[]>(`${this.baseURLAdmin}`);
@@ -19,27 +21,38 @@ export class AccountService {
   getVehiculos(): Observable<any[]> {
     return this.httpClient.get<any[]>(`${this.baseURLAdminVehiculos}`);
   }
+
+  register(info: any) {
+    this.httpClient.post("http://localhost:8080/users/register", info)
+      .subscribe(
+        respuesta => {
+          console.log(respuesta);
+        },
+        error => {
+          alert("Error");
+        }
+      );
+  }
+
+  registerAdmin(info: any) {
+    this.httpClient.post("http://localhost:8080/admin/register", info)
+      .subscribe(
+        respuesta => {
+          console.log(respuesta);
+        },
+        error => {
+          alert("Error");
+        }
+      );
+  }
+  actualizarCliente(cliente: any): Observable<any> {
+    const url = `${this.baseURLAdminActualizarCliente}`; 
+    return this.httpClient.put(url, cliente);
+  }
+  eliminarCliente(emailCliente: any): Observable<any> {
+    const url = `${this.baseURLAdminEliminarCliente}`; 
+    return this.httpClient.delete(url, emailCliente);
+  }
+
   
-  register (info : any) {
-    this.httpClient.post("http://localhost:8080/users/register",info)
-    .subscribe(
-      respuesta => {
-      console.log(respuesta)
-      },
-      _error =>{
-          alert("Error");
-      })
-    }
-
-  registerAdmin (info : any) {
-    this.httpClient.post("http://localhost:8080/admin/register",info)
-    .subscribe(
-      respuesta => {
-      console.log(respuesta)
-      },
-      _error =>{
-          alert("Error");
-      })
-    }
-
 }
