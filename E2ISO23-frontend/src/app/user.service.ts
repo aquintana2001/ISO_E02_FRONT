651333a43f376119d3e439436efd511de84410d5
Noel Fernández de Sevilla Sánchez <Noel.Fernandez@alu.uclm.es>
2023-10-29 12:27:31 +0100

    Implementación parcial consultar vehiculo

--- E2ISO23-frontend/src/app/user.service.ts
+++ E2ISO23-frontend/src/app/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ConsultarUsuariosComponent } from './consultar-usuarios/consultar-usuarios.component';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -7,40 +7,66 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AccountService {
-  private baseURLAdmin = "http://localhost:8080/admin/cliente";
+  private baseURLAdminClientes = "http://localhost:8080/admin/cliente";
   private baseURLAdminActualizarCliente = "http://localhost:8080/admin/actualizarCliente";
   private baseURLAdminEliminarCliente = "http://localhost:8080/admin/eliminarCliente";
+  private baseURLAdminDarBajaVehiculo = "http://localhost:8080/admin/darBajaVehiculo";
+
   private baseURLAdminVehiculos = "http://localhost:8080/admin/vehiculo";
 
   constructor(private httpClient: HttpClient) { }
 
   getClientes(): Observable<any[]> {
-    return this.httpClient.get<any[]>(`${this.baseURLAdmin}`);
+    const info = {
+      email: "pepe@pepe.com",
+      password: "Hola123*"
+    };
+    return this.httpClient.post<any[]>(`${this.baseURLAdminClientes}`, info);
   }
 
   getVehiculos(): Observable<any[]> {
-    return this.httpClient.get<any[]>(`${this.baseURLAdminVehiculos}`);
+    const info = {
+      email: "pepe@pepe.com",
+      password: "Hola123*"
+    };
+    return this.httpClient.post<any[]>(`${this.baseURLAdminVehiculos}`, info);
   }
 
-  register(info: any) {
-    this.httpClient.post("http://localhost:8080/users/register", info)
-      .subscribe(
-        respuesta => {
-          console.log(respuesta);
-        },
-        error => {
-          alert("Error");
-        }
-      );
+  register(info: any): Observable<any> {
+    return this.httpClient.post("http://localhost:8080/users/register", info);
   }
   
   actualizarCliente(cliente: any): Observable<any> {
     const url = `${this.baseURLAdminActualizarCliente}`; 
     return this.httpClient.put(url, cliente);
   }
+  actualizarVehiculo(vehiculo: any): Observable<any> {
+    const url = `${this.baseURLAdminVehiculos}`; 
+    return this.httpClient.put(url, vehiculo);
+  }
   eliminarCliente(emailCliente: any): Observable<any> {
     const url = `${this.baseURLAdminEliminarCliente}`; 
     return this.httpClient.delete(url, emailCliente);
+  }
+  eliminarVehiculo(id: any): Observable<any> {
+    const url = `${this.baseURLAdminDarBajaVehiculo}`;
+    const info = {
+      emailAdmin: "pepe@pepe.com",
+      password: "Hola123*",
+      id: id
+    }; 
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      body: {
+        emailAdmin: "pepe@pepe.com",
+        password: "Hola123*",
+        id: id
+      },
+    };
+    console.log(id);
+    return this.httpClient.delete(url, options);
   }
 
   
